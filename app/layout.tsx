407691cdbc1e0e30d78fa9eab06b25c1e7f178fc
Daniil Zahliadov <30547089+zagliadov@users.noreply.github.com>
2024-03-29 15:15:04 +0200

    speed insights test.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { openSans } from "@/app/ui/fonts";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 import CookiesConsent from "./ui/CookieConsent/CookieConsent";
@@ -28,6 +29,7 @@ export default function RootLayout({
       <GoogleTagManager gtmId="GTM-TTC44B3J" />
       <GoogleAnalytics gaId="G-M0HCH2F1PG" />
       <Analytics/>
+      <SpeedInsights/>
       <body
         className={`${openSans.variable} flex flex-col min-h-screen antialiased`}
       >
