c53aed3da5f02e33bed44b7d212233faf9d8f321
Daniil <portal.sid.grand2@gmail.com>
2024-04-12 07:20:09 +0300

    test lottie

--- app/ui/sections/components/LottieImage/LogoLottie.tsx
+++ app/ui/sections/components/LottieImage/LogoLottie.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Lottie from "react-lottie";
+import { useState, useEffect } from "react";
 import animationData from "@/public/logoU4D.json";
 
 const defaultOptions = {
@@ -11,13 +12,27 @@ const defaultOptions = {
     preserveAspectRatio: "xMidYMid slice",
   },
 };
+console.log(defaultOptions, 'defaultOptions')
 export default function LogoLottie() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    return () => {
+      setIsLoading(false);
+    }
+  }, []);
+
   return (
-    <div className="w-auto h-auto min-w-[1280px] flex justify-center max-w-[1280px] max-h-[720px] relative">
+    <div className="w-auto h-auto flex justify-center max-w-[1280px] max-h-[720px] relative">
       <Lottie options={defaultOptions} />
-      <h1 className="absolute top-[65%] text-center laptop:text-3xl text-2xl text-white leading-9">
-        We are united to bring the best traffic quality in digital world.
-      </h1>
+      {isLoading && (
+        <h1 className="absolute top-[65%] text-center laptop:text-3xl text-2xl text-white leading-9">
+          We are united to bring the best traffic quality in digital world.
+        </h1>
+      )}
+
     </div>
   );
 }
